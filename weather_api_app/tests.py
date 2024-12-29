2cc16bfbeed1bc8ffebd0795c80e68076d7c5dbf
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2024-12-29 22:30:02 +0800

    added tests

--- weather_api_app/tests.py
+++ weather_api_app/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class IndexTestCase(TestCase):
+    def test_homepage(self):
+        client = Client()
+        response = client.get("/")
+        self.assertTrue(response.status_code, 200)
+        self.assertContains(response, "submit")
+        self.assertContains(response, "Enter location")
+        
+    def test_location_search(self):
+        client = Client()
+        response = client.post("/", {"location": "New York"})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "New York")
+        self.assertContains(response, "Temperature")
